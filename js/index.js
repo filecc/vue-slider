468f2d73b48586d22b9edf792367cb06859a6396
filecc <fil.gia@live.it>
2023-03-29 15:31:27 +0200

    completed

--- js/index.js
+++ js/index.js
@@ -1,20 +1,80 @@
-import { setAppHeight, createChild } from "./utils.js";
+import { setAppHeight } from "./utils.js";
+
 
 (() => {
   setAppHeight();
-  window.addEventListener('resize', setAppHeight);
+  window.addEventListener("resize", setAppHeight);
 
-  const { createApp } = Vue
+  const { createApp } = Vue;
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        active: 'active',
+        images : [
+          {
+            image: "img/01.webp",
+            title: "Marvel's Spiderman Miles Morale",
+            text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+          },
+          {
+            image: "img/02.webp",
+            title: "Ratchet & Clank: Rift Apart",
+            text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+          },
+          {
+            image: "img/03.webp",
+            title: "Fortnite",
+            text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+          },
+          {
+            image: "img/04.webp",
+            title: "Stray",
+            text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+          },
+          {
+            image: "img/05.webp",
+            title: "Marvel's Avengers",
+            text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+          },
+        ],
+        imageIndex: 0,
+        imageToDisplay: '',
+        autoplay:  setInterval(this.nextSlide, 3000)
       }
+    },
+    methods: {
+      nextSlide() {
+        if (this.imageIndex === this.images.length - 1){
+          this.imageIndex = 0;
+        } else {
+          this.imageIndex +=1;
+        }
+        this.imageToDisplay = this.images[this.imageIndex].image
+      },
+      prevSlide(){
+        if (this.imageIndex === 0){
+          this.imageIndex = this.images.length - 1;
+        } else {
+          this.imageIndex -=1;
+        }
+        this.imageToDisplay = this.images[this.imageIndex].image
+      },
+      thumbsClick(id){
+        this.imageIndex = id;
+        this.imageToDisplay = this.images[id].image;
+      },
+      stopPlaying(){
+        clearInterval(this.autoplay);
+      },
+      restart(){
+        this.autoplay = setInterval(this.nextSlide, 3000)
+      }
+     
+    },
+    mounted() {
+     this.autoplay
     }
-  }).mount('#app')
-
-  
-
-
+    
+  }).mount("#app");
 })();
